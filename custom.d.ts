def6ee2df20cbb1cebb3f0be99339994d949bae1
Vergos1 <legendsmobajl4@gmail.com>
2024-01-10 16:26:17 +0200

    create directory with  vite

---
+++ custom.d.ts
@@ -0,0 +1,26 @@
+declare module '*.svg' {
+    import React = require('react');
+    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    const src: any;
+    export default src;
+}
+declare module '*.jpg' {
+    const content: any;
+    export default content;
+}
+declare module '*.png' {
+    const content: any;
+    export default content;
+}
+declare module '*.scss' {
+    const content: Record<string, string>;
+    export default content;
+}
+declare module '*.sass' {
+    const content: Record<string, string>;
+    export default content;
+}
+declare module '*.module.scss' {
+    const content: Record<string, string>;
+    export default content;
+}
